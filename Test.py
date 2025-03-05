9c7e69a07ed84ca8b0fa7ce1a09df3f3e3e55066
Sleepyboi <157728579+Sleepy-boiz@users.noreply.github.com>
2025-03-05 08:27:24 -0600

    Update

--- Test.py
+++ Test.py
@@ -1,6 +1,40 @@
+#this prints out the text "hello world"
 print("hello world")
 
-def test()
-    print("this is a function")
+#this is a variable. its a String they use words/characters
+#you can use = to define the variable
+Test_Var = "this is a Test Var that can be changed"
 
-test()
+#Prints the Test_Var
+print(Test_Var)
+
+#this changes the variable
+#it also makes it a equation 8 divided by 2 which equals 4
+Test_Var = 8/2
+
+#prints the Test_Var again to show the diffrence
+print(Test_Var)
+
+#you can also format the code like this
+print(f"8 divided by 2 is {Test_Var}")
+#f = format
+#"is the words" & the {} denote the variable to be put into the words
+
+
+
+
+#integers
+age = 25
+
+print(f"you are {age}")
+      
+age = age + 1
+
+print (f"you are one year older your now {age}")
+
+
+
+#floats (integers with decimals)
+price = 19.99
+
+print(f"the price is ${price}")
